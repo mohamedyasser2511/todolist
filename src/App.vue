7006a26bd76d9c4a16d51230ed16cf66a60e91df
Your Name <mohamedyasser251120000@gmail.com>
2025-01-30 12:36:16 +0200

    Updates V3

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <div class="logo">
-        <img style="width: 120px;" src="./assets/images/FoxSims.jpg" alt=""> 
+        <RouterLink to="/"><img style="width: 120px;" src="./assets/images/FoxSims.jpg" alt=""> </RouterLink>
       </div>
       <nav>
         <RouterLink to="/" class="nav-item">Home</RouterLink>
         <RouterLink to="/about" class="nav-item">About</RouterLink>
-        <RouterLink to="/contact" class="nav-item">Contact us</RouterLink>
+        <RouterLink to="/contact" class="nav-item">Contact us (extra)</RouterLink>
       </nav>
     </div>
   </header>
